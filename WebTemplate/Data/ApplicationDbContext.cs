9719d6932028e411905bd126343d0b3dcc61dbfc
SariItani <74187768+SariItani@users.noreply.github.com>
2025-01-01 14:45:04 +0200

    yay init

---
+++ WebTemplate/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebTemplate.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
